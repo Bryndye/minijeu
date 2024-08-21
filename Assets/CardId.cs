8bde0f284a7238044495c3b32398393f271c0d5a
Bryndye <hugocluzel17@hotmail.fr>
2024-08-21 13:48:46 +0200

    [DEV] Manager cards

---
+++ Assets/CardId.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class CardId : MonoBehaviour
+{
+    private int _cardId;
+    public int Cardid { get { return _cardId; } }
+
+}
---
+++ Assets/mini_jeu/_Scripts/Manager.cs
@@ -0,0 +1,83 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour
+{
+    public static Manager Instance;
+
+    public List<CardId> CardIds = new List<CardId>();
+
+    public List<CardId> CardsReturned = new List<CardId>();
+
+    public Action OnSameCards;
+    public Action OnDifferentCards;
+
+    private void Awake()
+    {
+        // Init Singleton
+        Instance = this;
+
+
+        OnSameCards += RemovePairCards;
+    }
+
+    #region List Cards
+    public void AddCard(CardId _cardiId)
+    {
+        if (!CardIds.Contains(_cardiId))
+        {
+            CardIds.Add(_cardiId);
+        }
+    }
+
+    public void RemoveCard(CardId _cardiId)
+    {
+        if (CardIds.Contains(_cardiId))
+        { 
+            CardIds.Remove(_cardiId);
+        }
+    }
+    #endregion
+
+    public void ReturnCard(CardId _cardiId)
+    {
+        if (CardsReturned.Contains(_cardiId))
+            return;
+
+        CardsReturned.Add(_cardiId);
+
+        if (CardsReturned.Count >= 2)
+        {
+            // verif id card
+            if (CardsReturned[0].Cardid == CardsReturned[1].Cardid)
+            {
+                // same cards
+                OnSameCards?.Invoke();
+            }
+            else
+            {
+                // different cards
+                OnDifferentCards?.Invoke();
+            }
+        }
+    }
+
+    public void RemovePairCards()
+    {
+        foreach (var _card in CardIds)
+        {
+            RemoveCard(_card);
+        }
+    }
+
+
+    public void VictoryCondition()
+    {
+        if (CardIds.Count <= 0)
+        {
+            // Victory
+        }
+    }
+}
