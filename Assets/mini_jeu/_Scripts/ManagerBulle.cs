1a89be5ba455509fb0b323a507aa5e8088163d8f
Metais Bastien <bastienmetais79@gmail.com>
2024-08-22 17:07:04 +0200

    polish au max

--- Assets/mini_jeu/_Scripts/ManagerBulle.cs
+++ Assets/mini_jeu/_Scripts/ManagerBulle.cs
@@ -8,15 +8,19 @@ public class ManagerBulle : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI _textMeshPro;
     private Manager manager;
+    [SerializeField] private GameObject _bubble;
     
     //Method to update the text of the bubble
     public void UpdateText(CardModel cardModel)
     {
+        _bubble.SetActive(false);
+        _bubble.SetActive(true);
         _textMeshPro.text = cardModel.textBulle.text;
     }
 
     public void Start()
     {
+        _bubble.SetActive(false);
         manager = Manager.Instance;
         manager.OnSameCards += UpdateText;        
     }
